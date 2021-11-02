9432db623476a386a343b8f99dd3e7de38f22f97
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2021-11-02 11:04:46 +0000

    Update weekly_bus_data.py

--- weekly_bus_data.py
+++ weekly_bus_data.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
-from pandas.io.json import json_normalize
-import numpy as np
 import datetime
 
+#set up variables to search for 7 days as URL only allows one day searches
 search_date=input('What date do want to search for? (YYYY-MM-DD)')
 day1 = datetime.datetime.strptime(search_date, '%Y-%m-%d')
 day2=day1 + datetime.timedelta(days=1)
@@ -16,16 +15,19 @@ day7=day6 + datetime.timedelta(days=1)
 
 days=[day1,day2,day3,day4,day5,day6]
 
+#format date to datetime
 d=[]
 for day in days:
     d1=day.strftime('%Y-%m-%d')
     d.append(d1)
 
+#creates url for each day
 request=[]
 for i in d:
     r1=requests.get('https://realjourneytime.azurewebsites.net/index.php?method=Journeys&fromCode=43000219503&toCode=43000414601&dateString='+i)
     request.append(r1)
 
+#cretes dataframe for each day
 times=[]
 for i in request:
     t=i.json()['JourneyTimes']
@@ -36,6 +38,7 @@ for i in times:
     df1=pd.DataFrame(i)
     df.append(df1)
 
+#turns each dataframe into single dataframe
 df_total = pd.concat(df)
 df_total.reset_index(drop=True,inplace=True)
 
@@ -43,6 +46,7 @@ df_total[['X','Y']]=df_total.ScheduledDepartureTime.str.split('T',expand=True)
 df_total[['time','Z']]=df_total.Y.str.split('Z',expand=True)
 df_total['Date']=df_total['X']+ ' '+ df_total['time']
 
+#converts journey time into seconds
 def convert(o_clock):
     h,m,s=o_clock.split(':')
     return int(m)*60+int(s)
@@ -51,13 +55,14 @@ df_total['real'] =min_col
 
 y=df_total.iloc[1]['ScheduledJourneyTime']
 
+#convert scheduled time to seconds
 def converty(line):
     h,m,s=line.split(':')
     return int(m)*60+int(s)
 newline=[converty(y)]
 
+#set up variable to early and late, 1 minute either side of scheduled journey time
 newnum = ''.join(str(e) for e in newline)
-
 late = int(newnum)+60
 early = int(newnum)-60
 ontime= late - early
@@ -70,10 +75,12 @@ latepercent = (x/percent)*100
 earlypercent = (y/percent)*100
 onpercent = (ontime/percent)*100
 
-print('The number of journeys on time was:',ontime,'(',round(onpercent,1),'%)')
-print('The number of journeys over the scheduled time was:',x,'(',round(latepercent,1),'%)')
-print('The number of journeys under the scheduled time was:',y,'(',round(earlypercent,1),'%)')
+#prints number of journeys on time, overtime and under time
+print(f'The number of journeys on time was: {ontime}({round(onpercent,1)}%)')
+print(f'The number of journeys over the scheduled time was: {x} ({round(latepercent,1)}%)')
+print(f'The number of journeys under the scheduled time was: {y}({round(earlypercent,1)}%)')
 
+#plots bar graph of journey times, with a red line indicating the scheduled journey time
 plt.bar(df_total['time'],df_total['real'], label='Actual Time')
 plt.axhline(newline, color='r', linestyle='--')
 plt.xlabel('Depature Time')
