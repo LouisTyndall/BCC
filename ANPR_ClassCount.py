6643f3013a32d83eeb9c959bcd0dc46dd152e4d0
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2021-11-05 16:51:46 +0000

    Update ANPR_ClassCount.py

--- ANPR_ClassCount.py
+++ ANPR_ClassCount.py
@@ -6,29 +6,14 @@ url = f'https://www.tfwm.onl/AWS_ClassCount.json?ApiKey={key}&earliest=22/09/202
 result = requests.get(url).json()
 df = pd.DataFrame(result['AWS_ClassCount']['kids'][n]['kids'] for n in result['AWS_ClassCount']['kids'])
 df['class_Count'] = df['class_Count'].astype(int)
-dfs = dict(tuple(df.groupby('camera_name')))
-y = df['camera_name'].tolist()
-y=list(set(y))
 
-values=[]
-for i in y:
-    value = dfs[i]
-    values.append(value)
-
-df1 = {}
-keys = range(len(y))
-for i in keys:
-        df1[i] = values[i]
-
-#print(df1[1])
 t = df['class'].tolist()
 vehicle_type=list(set(t))
 
-loc = df1[1]['camera_name'].iloc[1]
+df_new = df.loc[df['camera_name']== 'BW A457 Summer Hill Rd IB (City Centre)']
 
-print(f'For {loc}:')
 for i in vehicle_type:
-    x = df1[0].loc[df1[0]['class'] == i]
+    x = df_new.loc[df_new['class'] == i]
     num = x['class_Count'].sum()
     if num > 0:
         print(f'There are {num} of type {i}')
