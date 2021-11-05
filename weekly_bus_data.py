a9af4b9fa59597d609d9ef2f43c66a2796f13c5b
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2021-11-05 13:10:25 +0000

    Update weekly_bus_data.py

--- weekly_bus_data.py
+++ weekly_bus_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 import datetime
+from datetime import timedelta
 
-#set up variables to search for 7 days as URL only allows one day searches
 search_date=input('What date do want to search for? (YYYY-MM-DD)')
 day1 = datetime.datetime.strptime(search_date, '%Y-%m-%d')
 day2=day1 + datetime.timedelta(days=1)
@@ -15,19 +15,16 @@ day7=day6 + datetime.timedelta(days=1)
 
 days=[day1,day2,day3,day4,day5,day6]
 
-#format date to datetime
 d=[]
 for day in days:
     d1=day.strftime('%Y-%m-%d')
     d.append(d1)
 
-#creates url for each day
 request=[]
 for i in d:
     r1=requests.get('https://realjourneytime.azurewebsites.net/index.php?method=Journeys&fromCode=43000219503&toCode=43000414601&dateString='+i)
     request.append(r1)
 
-#cretes dataframe for each day
 times=[]
 for i in request:
     t=i.json()['JourneyTimes']
@@ -38,54 +35,58 @@ for i in times:
     df1=pd.DataFrame(i)
     df.append(df1)
 
-#turns each dataframe into single dataframe
-df_total = pd.concat(df)
-df_total.reset_index(drop=True,inplace=True)
+df = pd.concat(df)
+df.reset_index(drop=True,inplace=True)
 
-df_total[['X','Y']]=df_total.ScheduledDepartureTime.str.split('T',expand=True)
-df_total[['time','Z']]=df_total.Y.str.split('Z',expand=True)
-df_total['Date']=df_total['X']+ ' '+ df_total['time']
 
-#converts journey time into seconds
+#set up depature time column 
+df[['x','y']]=df.RealDepartureTime.str.split('T',expand=True)
+df[['DepartureTime','z']]=df.y.str.split('Z',expand=True)
+df = df.sort_values(by='DepartureTime',ascending=True)
+
 def convert(o_clock):
     h,m,s=o_clock.split(':')
     return int(m)*60+int(s)
-min_col=[convert(t) for t in df_total['RealJourneyTime']]
-df_total['real'] =min_col
+min_col=[convert(t) for t in df['RealJourneyTime']]
+df['RealTime'] =min_col
 
-y=df_total.iloc[1]['ScheduledJourneyTime']
-
-#convert scheduled time to seconds
-def converty(line):
-    h,m,s=line.split(':')
+def convert(o_clock):
+    h,m,s=o_clock.split(':')
     return int(m)*60+int(s)
-newline=[converty(y)]
+min_col=[convert(t) for t in df['ScheduledJourneyTime']]
+df['ScheduledTime'] =min_col
 
-#set up variable to early and late, 1 minute either side of scheduled journey time
-newnum = ''.join(str(e) for e in newline)
-late = int(newnum)+60
-early = int(newnum)-60
-ontime= late - early
-x=sum(df_total['real']>int(late))
-y = sum(df_total['real'] > int(early))
+df['ScheduledTimeOver']=df['ScheduledTime'] + 60
+df['ScheduledTimeUnder']=df['ScheduledTime'] - 60
+df = df[(df['RealTime'] < 1500)]
 
-percent = ontime+x+y
+df_under = df.loc[(df['RealTime']) <= df['ScheduledTimeUnder']]
+df_over = df.loc[(df['RealTime']) >= df['ScheduledTimeOver']]
 
-latepercent = (x/percent)*100
-earlypercent = (y/percent)*100
-onpercent = (ontime/percent)*100
+total=len(df)
+num_over = len(df_over.index)
+num_under = len(df_under.index)
+num_ontime= len(df) - num_over - num_under
 
-#prints number of journeys on time, overtime and under time
-print(f'The number of journeys on time was: {ontime}({round(onpercent,1)}%)')
-print(f'The number of journeys over the scheduled time was: {x} ({round(latepercent,1)}%)')
-print(f'The number of journeys under the scheduled time was: {y}({round(earlypercent,1)}%)')
 
-#plots bar graph of journey times, with a red line indicating the scheduled journey time
-plt.bar(df_total['time'],df_total['real'], label='Actual Time')
-plt.axhline(newline, color='r', linestyle='--')
+over_percent = (num_over/total)*100
+under_percent = (num_under/total)*100
+ontime_percent = (num_ontime/total)*100
+
+df_over['Difference'] = df_over['RealTime'] - df_over['ScheduledTime']
+x = df_over['Difference'].mean()
+
+print('Average time over scheduled was', (x/60))
+print(f'The total number of journeys was {total}')
+print(f'The number of journeys on time was: {round(num_ontime)} ({round(ontime_percent,1)}%)')
+print(f'The number of journeys late time was: {round(num_over)} ({round(over_percent,1)}%)')
+print(f'The number of journeys early time was: {round(num_under)} ({round(under_percent,1)}%)')
+
+plt.bar(df['DepartureTime'],df['RealTime'], label='Actual Time')
+plt.plot(df['DepartureTime'],df['ScheduledTime'],'r', label='Scheduled Time')
 plt.xlabel('Depature Time')
-plt.ylabel('Journey Time (m)')
-plt.title('Bus Journey Time, actual vs predicted')
+plt.ylabel('Journey Time (s)')
+plt.title(f'Bus Journey Time, actual vs predicted for w/c {search_date}')
 plt.legend()
 fig = plt.gcf()
 fig.set_size_inches(40, 15)
