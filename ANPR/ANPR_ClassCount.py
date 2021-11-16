f264b7d87b08feda06d2b009e65988557c882d37
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2021-11-16 15:13:36 +0000

    Update ANPR_ClassCount.py

--- ANPR/ANPR_ClassCount.py
+++ ANPR/ANPR_ClassCount.py
@@ -2,33 +2,24 @@ import requests
 from passwords import *
 import pandas as pd
 
+#Potential to add input() to allow search date
 url = f'https://www.tfwm.onl/AWS_ClassCount.json?ApiKey={key}&earliest=01/11/2021&latest=02/11/2021'
 result = requests.get(url).json()
+
+#Format data to dataframe
 df = pd.DataFrame(result['AWS_ClassCount']['kids'][n]['kids'] for n in result['AWS_ClassCount']['kids'])
 df['class_Count'] = df['class_Count'].astype(int)
-dfs = dict(tuple(df.groupby('camera_name')))
-y = df['camera_name'].tolist()
-y=list(set(y))
 
-values=[]
-for i in y:
-    value = dfs[i]
-    values.append(value)
+#Select location. Can add input() to allow user to select location
+df_new = df.loc[df['camera_name'] == 'SW A4032 Churchbridge SB (Oldbury)']
 
-df1 = {}
-keys = range(len(y))
-for i in keys:
-        df1[i] = values[i]
-
-#print(df1[1])
+#Creates a list of each type of vehicle
 t = df['class'].tolist()
 vehicle_type=list(set(t))
 
-#loc = df1[8]['camera_name'].iloc[1]
-
-#print(f'For {loc}:')
+#For loop to count, and print, the number of each vehicle type
 for i in vehicle_type:
-#    x = df1[8].loc[df1[8]['class'] == i]
-    num = df1['class_Count'].sum()
+    df_new = df_new.loc[df_new['class'] == i]
+    num_vehicles = df_new['class_Count'].sum()
     if num > 0:
-        print(f'There are {num} number of {i}')
+        print(f'There are {num_vehicles} for {i}')
