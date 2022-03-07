7c3bd263bf2f522022635e05cac87ef944fee4ab
radforaw <andy@andyradford.co.uk>
2022-03-07 09:38:07 +0000

    just checking beta is updated

--- CAZ anonymised vehicle data/quickcsvcheck.py
+++ CAZ anonymised vehicle data/quickcsvcheck.py
@@ -20,8 +20,16 @@ def by_vehicle(d):
 		if read_siemens_time(n['Capture Date']).date()==datetime.datetime(2022,2,28,0,0,0).date():
 			res[n['Hashed VRN']].append([n['RSE Id'],read_siemens_time(n['Capture Date']),n['Direction of Travel']])
 	return res
-	
-if __name__=='__main__':
+
+def by_camera(d):
+	res=defaultdict(list)
+	for n in d:
+		res[(n['RSE Id'],n['Direction of Travel'])].append(read_siemens_time(n['Capture Date']))
+	return res
+
+
+
+def basicexample():
 	d=load_data()
 	x=by_vehicle(d)
 	tot=0
@@ -43,4 +51,32 @@ if __name__=='__main__':
 		if n['Vehicle Recognised']!='true':
 			cr+=1
 	print (cr/veh)
-			
+
+if __name__=='__main__':
+	import numpy
+	d=load_data()
+	x=by_camera(d)
+	for n in x:
+		print (n,len(x[n]))
+	cam=('CAZ001','Approaching')
+	data=sorted(x[cam])
+	tm=data[0]
+	end=data[-1]
+	t=defaultdict(list)
+	for n in data:
+		tmp = n - datetime.timedelta(minutes=n.minute % 5,
+                             seconds=n.second,
+                             microseconds=n.microsecond)
+
+		t[tmp].append(n)
+	res={}
+	for n in t:
+		print (t[n])
+		#res[n]=[int(sum([(t[n][a+1]-t[n][a]).total_seconds() for a in range(len(t[n])-1)])/len(t[n])-1),len(t[n]),300/len(t[n])]
+		res[n]=[numpy.std([(t[n][a+1]-t[n][a]).total_seconds() for a in range(len(t[n])-1)]),len(t[n]),300/len(t[n])]
+	
+	for n in res:
+		print (n,res[n])
+	import matplotlib.pyplot as plt
+	plt.plot([res[a][1] for a in res],[res[b][0] for b in res],"o")
+	plt.show()
