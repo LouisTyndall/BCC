a40328b29c53534cc0d2173cbd62f090b4be496d
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2023-02-01 11:52:07 +0000

    Update RTEM_latest_speed_per_site.py

--- RTEM - Louis/RTEM_latest_speed_per_site.py
+++ RTEM - Louis/RTEM_latest_speed_per_site.py
@@ -5,10 +5,13 @@ from datetime import date
 import requests
 import password as p
 
+#create a variable of todays date
 key=p.key
 today = date.today()
 end= today+datetime.timedelta(days=1)
 print(today)
+
+#Get request with todays date and the desired location
 site=input('Enter site code')
 url='http://bcc.opendata.onl/rtem_csv.json?Earliest='+str(today)+'&Latest='+str(end)+'&scn='+site+'&ApiKey='+key 
 result=requests.get(url).json()
@@ -16,9 +19,12 @@ df = pd.DataFrame(result['RTEM_CSVs']['kids'][n]['kids'] for n in result['RTEM_C
 df.Date = pd.to_datetime(df.Date)
 df['AverageSpeed']=df['AverageSpeed'].astype(int)
 
+#create a variable for the latest time stamp and the latest speed
 Speed = df.iloc[-1]['AverageSpeed']
 when = df.iloc[-1]['Date']
 
+#Option to only print the variable if the speed is a certain level
 #if 0< Speed <9
 
+#Print the result
 print('The latest average speed recorded was',Speed,'kmph at',when)
