757e4b3f7e1035d279b2c08105f77038b046caca
LouisTyndall <93523796+LouisTyndall@users.noreply.github.com>
2023-02-01 11:24:54 +0000

    Update RTEM_count_speed_graph.py

--- RTEM - Louis/RTEM_count_speed_graph.py
+++ RTEM - Louis/RTEM_count_speed_graph.py
@@ -5,6 +5,7 @@ import requests
 from passwords import key,password
 import matplotlib.pyplot as plt
 
+#Set up search parameters, including date and the number of days to search for.
 start = input('Date:')
 start = datetime.datetime.strptime(start, '%Y-%m-%d')
 day = input('Number of days:')
@@ -13,6 +14,7 @@ sites= ['R0101L1',]#'R0101L2']
 
 dfs=[]
 for site in sites:
+    #Get request for each site, check that the response is 200
     url='http://bcc.opendata.onl/rtem_csv.json?Earliest='+str(start)+'&Latest='+str(end)+'&scn='+site+'&ApiKey='+key 
     result=requests.get(url)
     if result.status_code != 200:
@@ -22,6 +24,7 @@ for site in sites:
     if len(result['RTEM_CSVs']) == 1:
         ('Empty data frame')
         continue
+    #if there is data, format and resample to 1H bins.
     df=pd.DataFrame(result['RTEM_CSVs']['kids'][n]['kids'] for n in result['RTEM_CSVs']['kids'])
     df['Date'] =pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
@@ -29,7 +32,8 @@ for site in sites:
     df['AverageSpeed'] = df['AverageSpeed'].astype(float)
     df = df.resample('1H').mean()
     dfs.append(df)
-    
+
+#Combine the dataframes, and add a new column for the mean speed.
 df = pd.concat(dfs)
 df.loc['mean'] = df.mean()
 mean = df.values[-1].tolist()
@@ -38,6 +42,7 @@ df = df.iloc[:-1,:]
 plt.figure(figsize=(35,20))
 fig, ax1 = plt.subplots()
 
+#Plot the graph
 ax2 = ax1.twinx()
 ax1.bar(df.index,df['Total'],color='red',align='edge', width=(df.index[1]-df.index[0])*0.8)
 ax2.plot(df.index,df['AverageSpeed'], linestyle="",marker="o")
